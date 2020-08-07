f94c7da31c1e868b924646ed1b0e583250c3251e
石浩博 <42900775+626sy@users.noreply.github.com>
2020-08-07 10:42:53 +0800

    初始化饿了么后台管理

--- src/com/neusoft/domain/Admin.java
+++ src/com/neusoft/domain/Admin.java
@@ -1,7 +1,6 @@
 package com.neusoft.domain;
 
-/**
- * @author Eric Lee
+/*
  * @date 2020/8/7 09:44
  * 用户表实体类
  */
--- src/com/neusoft/test/TestAdmin.java
+++ src/com/neusoft/test/TestAdmin.java
@@ -4,10 +4,7 @@ import com.neusoft.dao.AdminDao;
 import com.neusoft.dao.Impl.AdminDaoImpl;
 import com.neusoft.domain.Admin;
 
-/**
- * @author Eric Lee
- * @date 2020/8/7 10:01
- */
+
 public class TestAdmin {
     public static void main(String[] args) {
         AdminDao adminDao = new AdminDaoImpl();
