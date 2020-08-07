08d32703c0477d56fb177f8ae7b55ece84b8e5f6
石浩博 <42900775+626sy@users.noreply.github.com>
2020-08-07 09:36:06 +0800

    main测试

---
+++ src/com/neusoft/Main.java
@@ -0,0 +1,11 @@
+package com.neusoft;
+
+/**
+ * @author shihaobo
+ * @date 2020/8/7 9:35
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
